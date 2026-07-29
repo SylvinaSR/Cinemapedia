d9a0bd38f3984460c90730a5dc9ac98adb02f374
Sylvie Salazar <sylvina_srosales@hotmail.com>
2026-07-29 13:33:51 -0600

    feat: second part - human format for voteAverage and popularity

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helper/human_format.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -106,13 +107,17 @@ class _Slide extends StatelessWidget {
           ),
           SizedBox(height: 5),
           SizedBox(width: 150, child: Text(movie.title, maxLines: 2, style: textStyle.titleSmall,)),
-          Row(
-            children: [
-              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800,),
-              SizedBox(width: 3,),
-              Text('${movie.voteAverage}', style: textStyle.bodyMedium?.copyWith(color: Colors.yellow.shade800),),
-              Text('${movie.popularity}', style: textStyle.bodySmall,)
-            ],
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800,),
+                SizedBox(width: 3,),
+                Text(HumanFormat.number(movie.voteAverage), style: textStyle.bodyMedium?.copyWith(color: Colors.yellow.shade800),),
+                Spacer(),
+                Text(HumanFormat.number(movie.popularity), style: textStyle.bodySmall,)
+              ],
+            ),
           )
         ],
       ),
