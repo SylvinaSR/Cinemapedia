7be7956a56ef512e8263e41c5dfc1f43146876b8
Sylvie Salazar <sylvina_srosales@hotmail.com>
2026-08-04 16:49:34 -0600

    feat: third part - create structure to get actors

---
+++ lib/infraestructure/datasources/actor_moviedb_datasource.dart
@@ -0,0 +1,28 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/actors_datasource.dart';
+import 'package:cinemapedia/domain/entities/actor.dart';
+import 'package:cinemapedia/infraestructure/mappers/actor_mapper.dart';
+import 'package:cinemapedia/infraestructure/models/moviedb/cast_response.dart';
+import 'package:dio/dio.dart';
+
+class ActorMoviedbDatasource extends ActorsDatasource {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Environment.movieApiKey,
+        'language': 'es-MX',
+      },
+    ),
+  );
+
+  @override
+  Future<List<Actor>> getActorsByMovie(String movieId) async {
+    final response = await dio.get('movie/$movieId/credits');
+    final castResponse = CastResponse.fromJson(response.data);
+    final List<Actor> actors = castResponse.cast
+        .map((e) => ActorMapper.castToEntity(e))
+        .toList();
+    return actors;
+  }
+}
