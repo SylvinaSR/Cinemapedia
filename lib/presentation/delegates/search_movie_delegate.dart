50a6793961761774a2dcdacfcb4712d75d49c109
Sylvie Salazar <sylvina_srosales@hotmail.com>
2026-08-05 17:29:18 -0600

    feat: fourth part - show movies in search

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -46,12 +46,56 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
         final movies = snapshot.data ?? [];
         return ListView.builder(
           itemCount: movies.length,
-          itemBuilder: (context, index) {
-            final movie = movies[index];
-            return ListTile(title: Text(movie.title));
-          },
+          itemBuilder: (context, index) => _MovieItem(movie: movies[index]),
         );
       },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsetsGeometry.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: [
+          SizedBox(
+            width: size.width * 0.2,
+            child: ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(20),
+              child: Image.network(
+                errorBuilder: (_, _, _) => Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8PgItrmj9SWhclDBAmjlyFsC4w9tEKTcvDTbc2puQtQ&s',
+                ),
+                movie.posterPath,
+                loadingBuilder: (context, child, loadingProgress) =>
+                    FadeIn(child: child),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10,),
+          SizedBox(
+            width: size.width * 0.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(movie.title, style: textStyles.titleMedium,),
+                movie.overview.length > 100
+                ? Text('${movie.overview.substring(0,100)}...')
+                : Text(movie.overview)
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
